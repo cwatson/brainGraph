0f3c0b1eccf3c32fa0eb5027bc989ae1a8ddd01a
Chris Watson <cwatson@cwatson.dtdns.net>
2014-05-23 18:00:24 -0400

    Added a function to set graph and vertex attributes.

---
+++ R/set_brainGraph_attributes.R
@@ -0,0 +1,44 @@
+#' Set a number of graph and vertex attributes useful in MRI analyses
+#'
+#' This function will set a number of graph and vertex attributes of a given
+#' igraph object. It is required that a community structure has already been
+#' determined, and \code{\link{part.coeff}} and
+#' \code{\link{within.module.deg.z.score}} have been calculated.
+#'
+#' @param g A graph
+#' @export
+#'
+#' @return g A copy of the same graph, with the attributes added.
+
+set.brainGraph.attributes <- function(g, vcols, ecols, PC, z, rand=FALSE) {
+  g$conn.comp <- max(clusters(g)$csize)
+  g$num.tri <- graph.motifs(g)[4]
+  V(g)$degree <- degree(g)
+
+  V(g)$btwn.cent <- centralization.betweenness(g)$res
+  V(g)$ev.cent <- centralization.evcent(g)$vector
+
+  t <- transitivity(g, type='local')
+  V(g)$transitivity <- ifelse(is.nan(t), 0, t)
+  g$Cp <- transitivity(g)
+
+  g$Lp <- average.path.length(g)
+
+  g$assortativity <- assortativity.degree(g)
+
+  g$g.eff <- global.eff(g)
+  V(g)$l.eff <- local.eff(g)
+
+  if (rand == TRUE) {
+    deg.thresh <- vcount(g) - ceiling(0.1 * vcount(g))
+    g$mod <- max(fastgreedy.community(g)$modularity)
+    g$rich <- rich.club.coeff(g, sort(degree(g))[deg.thresh])$coeff
+  } else {
+    V(g)$color <- vcols
+    E(g)$color <- ecols
+    V(g)$PC <- PC
+    V(g)$z <- z
+  }
+
+  g
+}
