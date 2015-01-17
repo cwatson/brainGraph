549f99e0ffca09004986e39128cc7c7267ea12e1
Chris Watson <cwatson@cwatson.dtdns.net>
2015-01-16 20:45:58 -0500

    Updated package to v0.17.0 * Created function to do a vertex-wise t-test for a vertex measure * Created function to do permutation testing based on max t-statistic

---
+++ R/group_graph_diffs.R
@@ -0,0 +1,36 @@
+#' Do between-group t-tests at each vertex for a given graph measure
+#'
+#' This function takes two lists of graphs (the length of each equaling the
+#' number of subjects per group) and performs a 2-sample t-test at each vertex
+#' for a given network measure (e.g. vertex degree).
+#'
+#' @param g1 A list of igraph graph objects for group 1
+#' @param g2 A list of igraph graph objects for group 2
+#' @param measure A character string of the measure to test
+#'
+#' @export
+#'
+#' @return A graph with vertex attributes: \emph{size2} equals the t-statistic;
+#' \emph{size} is transformed to be positive values for visualization; \emph{p}
+#' is equal to \eqn{1 - p}; and \emph{p.adj} is 1 - the FDR-adjusted p-value
+#'
+#' @seealso \code{\link[stats]{t.test}, \link[stats]{p.adjust},
+#' \link{range.transform}}
+#' @author Christopher G. Watson, \email{cgwatson@@bu.edu}
+
+group.graph.diffs <- function(g1, g2, measure) { 
+  Nv <- vcount(g1[[1]])
+  meas1 <- sapply(g1, vertex_attr, measure)
+  meas2 <- sapply(g2, vertex_attr, measure)
+  t.stats <- mapply(function(x, y) t.test(meas1[x, ], meas2[y, ]),
+                    seq_len(Nv), seq_len(Nv), SIMPLIFY=F)
+
+  g.diffs <- g1[[1]]
+  V(g.diffs)$size2 <- sapply(t.stats, function(x) x$statistic)
+  V(g.diffs)$size <- range.transform(V(g.diffs)$size2, 0, 20)
+  V(g.diffs)$p <- sapply(t.stats, function(x) x$p.value)
+  V(g.diffs)$p.adj <- p.adjust(V(g.diffs)$p, 'fdr')
+  V(g.diffs)$p <- 1 - V(g.diffs)$p
+  V(g.diffs)$p.adj <- 1 - V(g.diffs)$p.adj
+  return(g.diffs)
+}
---
+++ R/permute_groups.R
@@ -0,0 +1,54 @@
+#' Permutation test of groupwise differences in a graph measure
+#'
+#' This function performs a permutation test looking at max t-statistic for a
+#' given vertex-level graph attribute (e.g. vertex degree). This is the same
+#' principle as that of Nichols & Holmes (2001).
+#'
+#' @param g1 A list of igraph graph objects for group 1
+#' @param g2 A list of igraph graph objects for group 2
+#' @param alpha The significance level (default: 0.05)
+#' @param num.perms The number of permutations (default: 1e3)
+#' @param measure A character string of the measure to test
+#'
+#' @export
+#'
+#' @return A list with the following elements:
+#' \item{g}{A graph representing group differences, with \emph{p.perm} included
+#' as a vertex-level attribute (it is actually 1 - \emph{p.perm})}
+#' \item{p.max}{The proportion of the number of times the maximum t-statistic of
+#' the permuted groups was greater than the observed maximum t-statistic}
+#' \item{thresh}{The (1 - alpha) \%ile maximum t-statistic of all permuted values}
+#'
+#' @seealso \code{\link{sample}, \link{group.graph.diffs}}
+#' @author Christopher G. Watson, \email{cgwatson@@bu.edu}
+#' @references Nichols TE & Holmes AP (2001). \emph{Nonparametric permutation
+#' tests for functional neuroimaging: A primer with examples.} Human Brain
+#' Mapping, 15(1):1-25.
+#' @examples
+#' \dontrun{
+#' g.diff <- permute.groups(g.wt[[1]][[3]], g.wt[[2]][[3]], measure='degree')
+#' }
+
+permute.groups <- function(g1, g2, alpha=0.05, num.perms=1e3, measure) {
+  combined <- c(g1, g2)
+  g.diffs <- group.graph.diffs(g1, g2, measure)
+  max.observed <- max(V(g.diffs)$size2)
+  max.rand <- vector(length=num.perms)
+
+  n1 <- length(g1)
+  n.all <- length(combined)
+  max.rand <- foreach(i=seq_len(num.perms), .combine='c') %dopar% {
+    shuffled <- sample(n.all)
+    g1.rand <- combined[shuffled[1:n1]]
+    g2.rand <- combined[shuffled[(n1 + 1):n.all]]
+
+    max(V(group.graph.diffs(g1.rand, g2.rand, measure))$size2)
+  }
+
+  p.max <- sum(abs(max.rand) >= abs(max.observed)) / num.perms
+  thresh <- sort(max.rand)[(1 - alpha) * num.perms]
+  Nv <- vcount(g.diffs)
+  V(g.diffs)$p.perm <- 1 - sapply(seq_len(Nv), function(x)
+                                  sum(max.rand >= V(g.diffs)$size2[x]) / num.perms)
+  return(list(g=g.diffs, p.max=p.max, thresh=thresh))
+}
