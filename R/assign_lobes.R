4277b20b152ed09721551080d220020658ab9e4c
Chris Watson <Christopher.Watson@childrens.harvard.edu>
2015-05-26 15:07:01 -0400

    Updated package to v0.24.2 * Fixed bug when doing permutations * Made some changes to "permute.group"

--- R/assign_lobes.R
+++ R/assign_lobes.R
@@ -5,22 +5,35 @@
 #' attribute \emph{circle.layout} for plotting circular graphs.
 #'
 #' @param g An igraph graph object
-#' @param atlas.dt A list with data for a specific atlas
+#' @param atlas.dt A data table for a specific atlas
 #'
 #' @return An igraph graph object with additional vertex attributes \emph{lobe},
 #' \emph{lobe.hemi}, and \emph{circle.layout}
 #' @author Christopher G. Watson, \email{cgwatson@@bu.edu}
 
 assign_lobes <- function(g, atlas.dt) {
-  lobe <- hemi <- index <- NULL
+  lobe <- hemi <- NULL
   atlas <- g$atlas
   vorder <- match(V(g)$name, atlas.dt$name)
   V(g)$lobe <- atlas.dt[vorder, as.numeric(lobe)]
   V(g)$lobe.hemi <- as.numeric(atlas.dt[vorder, interaction(lobe, hemi)])
   V(g)$hemi <- as.character(atlas.dt[vorder, hemi])
 
-  if (atlas %in% c('dkt', 'dk')) {
-    third <- 'Cingulate'
+  if (atlas %in% c('dkt', 'dk', 'destrieux')) {
+    counts <- atlas.dt[, length(name), by=c('lobe', 'hemi')][order(lobe)]$V1
+    V(g)$circle.layout <-
+      c(which(V(g)$lobe == 1 & V(g)$hemi == 'L'),
+        which(V(g)$lobe == 5 & V(g)$hemi == 'L'),
+        which(V(g)$lobe == 6 & V(g)$hemi == 'L'),
+        which(V(g)$lobe == 3 & V(g)$hemi == 'L'),
+        which(V(g)$lobe == 2 & V(g)$hemi == 'L'),
+        which(V(g)$lobe == 4 & V(g)$hemi == 'L'),
+        which(V(g)$lobe == 4 & V(g)$hemi == 'R'),
+        which(V(g)$lobe == 2 & V(g)$hemi == 'R'),
+        which(V(g)$lobe == 3 & V(g)$hemi == 'R'),
+        which(V(g)$lobe == 6 & V(g)$hemi == 'R'),
+        which(V(g)$lobe == 5 & V(g)$hemi == 'R'),
+        which(V(g)$lobe == 1 & V(g)$hemi == 'R'))
     
   } else if (atlas == 'aal90') {
     V(g)$circle.layout <- with(atlas.dt, c(frontal.lh, insula.lh, limbic.lh,
@@ -35,36 +48,7 @@ assign_lobes <- function(g, atlas.dt) {
                                              occipital.lh, occipital.rh,
                                              parietal.rh, temporal.rh, scgm.rh,
                                              cingulate.rh, insula.rh, frontal.rh))
-  } else if (atlas == 'destrieux') {
-    third <- 'Limbic'
   }
-
-  counts <- atlas.dt[, length(name), by=c('lobe', 'hemi')][order(lobe)]$V1
-  V(g)$circle.layout <-
-    c(which(V(g)$lobe == 1 & V(g)$hemi == 'L'),
-      which(V(g)$lobe == 5 & V(g)$hemi == 'L'),
-      which(V(g)$lobe == 6 & V(g)$hemi == 'L'),
-      which(V(g)$lobe == 3 & V(g)$hemi == 'L'),
-      which(V(g)$lobe == 2 & V(g)$hemi == 'L'),
-      which(V(g)$lobe == 4 & V(g)$hemi == 'L'),
-      which(V(g)$lobe == 4 & V(g)$hemi == 'R'),
-      which(V(g)$lobe == 2 & V(g)$hemi == 'R'),
-      which(V(g)$lobe == 3 & V(g)$hemi == 'R'),
-      which(V(g)$lobe == 6 & V(g)$hemi == 'R'),
-      which(V(g)$lobe == 5 & V(g)$hemi == 'R'),
-      which(V(g)$lobe == 1 & V(g)$hemi == 'R'))
-#    c(atlas.dt[lobe == 'Frontal' & hemi == 'L', index[vorder]][seq_len(counts[1])],
-#      atlas.dt[lobe == 'Insula' & hemi == 'L', index[vorder]][seq_len(counts[9])],
-#      atlas.dt[lobe == third & hemi == 'L', index[vorder]][seq_len(counts[11])],
-#      atlas.dt[lobe == 'Temporal' & hemi == 'L', index[vorder]][seq_len(counts[5])],
-#      atlas.dt[lobe == 'Parietal' & hemi == 'L', index[vorder]][seq_len(counts[3])],
-#      atlas.dt[lobe == 'Occipital' & hemi == 'L', index[vorder]][seq_len(counts[7])],
-#      atlas.dt[lobe == 'Occipital' & hemi == 'R', index[vorder]][seq_len(counts[8])],
-#      atlas.dt[lobe == 'Parietal' & hemi == 'R', index[vorder]][seq_len(counts[4])],
-#      atlas.dt[lobe == 'Temporal' & hemi == 'R', index[vorder]][seq_len(counts[6])],
-#      atlas.dt[lobe == third & hemi == 'R', index[vorder]][seq_len(counts[12])],
-#      atlas.dt[lobe == 'Insula' & hemi == 'R', index[vorder]][seq_len(counts[10])],
-#      atlas.dt[lobe == 'Frontal' & hemi == 'R', index[vorder]][seq_len(counts[2])])
 
   g
 }
--- R/permute_group.R
+++ R/permute_group.R
@@ -5,6 +5,10 @@
 #' function is intended for cortical thickness networks (in which there is only
 #' one graph per group), but is not restricted to that type of data.
 #'
+#' The \emph{graph} "level" will calculate modularity (Louvain algorithm),
+#' clustering coefficient, average path length, degree assortativity, global
+#' efficiency, and lobe assortativity.
+#'
 #' The \emph{lobe} "level" is intended to test for group differences in
 #' inter-lobar connections, e.g. from the temporal lobe to the rest of the
 #' brain.
@@ -18,8 +22,9 @@
 #' @param num.subjs A vector of length 2 indicating group sizes
 #' @param num.perms The number of permutations to perform (default: 1e3)
 #' @param level A character string for the attribute level to calculate
-#' @param atlas.list List containing the specific atlas data
 #' differences; either 'graph', 'vertex', 'lobe', or 'asymmetry'
+#' @param atlas Character string of the atlas name
+#' @param atlas.dt Data table containing the specific atlas data
 #' @export
 #'
 #' @return A data table with values for group differences in modularity, global
@@ -31,26 +36,27 @@
 #' @author Christopher G. Watson, \email{cgwatson@@bu.edu}
 #' @examples
 #' \dontrun{
-#' out <- permute.group(densities, m$resids, summary(covars$Group), 1e3, 'graph')
-#' out <- permute.group(densities, m$resids, summary(covars$Group), 1e3, 'lobe',
-#' atlas.list)
+#' out <- permute.group(densities, m$resids, summary(covars$Group), 1e3, 'graph',
+#'   atlas='dk', atlas.dt)
+#' out <- permute.group(densities, m$resids, summary(covars$Group), 1e3, 'vertex')
 #' }
 
 permute.group <- function(densities, resids, num.subjs, num.perms=1e3,
                           level=c('graph', 'vertex', 'lobe', 'asymmetry'),
-                          atlas.list=NULL) {
-  n1 <- num.subjs[1]
+                          atlas, atlas.dt=NULL) {
+  n1 <- as.numeric(num.subjs[1])
   n.all <- sum(num.subjs)
 
   level <- match.arg(level)
-  out <- foreach(i=seq_len(num.perms), .combine='rbind') %dopar% {
+  out <- foreach(i=seq_len(num.perms), .combine='rbind',
+                 .packages='plyr', .export='assign_lobes') %dopar% {
     shuffled <- sample(n.all)
     corrs1 <- lapply(densities, function(x)
                      corr.matrix(resids[shuffled[1:n1]][, !'Group', with=F],
                                  density=x))
     corrs2 <- lapply(densities, function(x)
-                     corr.matrix(resids[shuffled[(n1 +1):n.all]][, !'Group', with=F],
-                                 density=x))
+                     corr.matrix(resids[shuffled[(n1 +1):n.all]][, !'Group',
+                                 with=F], density=x))
     g1 <- lapply(corrs1, function(x)
                  simplify(graph.adjacency(x$r.thresh, mode='undirected')))
     g2 <- lapply(corrs2, function(x)
@@ -61,49 +67,48 @@ permute.group <- function(densities, resids, num.subjs, num.perms=1e3,
                           g1, g2, SIMPLIFY=T)
       tmp <- as.data.table(cbind(densities, t(btwn.diff)))
 
-    } else if (level == 'graph') {
-      mod1 <- sapply(g1, function(x) modularity(cluster_louvain(x)))
-      mod2 <- sapply(g2, function(x) modularity(cluster_louvain(x)))
-      mod.diff <- mod1 - mod2
-      Cp1 <- sapply(g1, transitivity, type='localaverage')
-      Cp2 <- sapply(g2, transitivity, type='localaverage')
-      Cp.diff <- Cp1 - Cp2
-      Lp1 <- sapply(g1, average.path.length)
-      Lp2 <- sapply(g2, average.path.length)
-      Lp.diff <- Lp1 - Lp2
-      assortativity1 <- sapply(g1, assortativity.degree)
-      assortativity2 <- sapply(g2, assortativity.degree)
-      assort.diff <- assortativity1 - assortativity2
-      E.global1 <- sapply(g1, graph.efficiency, 'global')
-      E.global2 <- sapply(g2, graph.efficiency, 'global')
-      E.global.diff <- E.global1 - E.global2
+    } else {
+      g1 <- lapply(g1, set_graph_attr, 'atlas', atlas)
+      g2 <- lapply(g2, set_graph_attr, 'atlas', atlas)
+      g1 <- lapply(g1, assign_lobes, atlas.dt)
+      g2 <- lapply(g2, assign_lobes, atlas.dt)
 
-      g1 <- lapply(g1, assign_lobes, atlas.list)
-      g2 <- lapply(g2, assign_lobes, atlas.list)
-      assort.lobe1 <- sapply(g1, function(x) assortativity_nominal(x, V(x)$lobe))
-      assort.lobe2 <- sapply(g2, function(x) assortativity_nominal(x, V(x)$lobe))
-      assort.lobe.diff <- assort.lobe1 - assort.lobe2
-      tmp <- data.table(density=densities, mod=mod.diff, E.global=E.global.diff,
-                 Cp=Cp.diff, Lp=Lp.diff, assortativity=assort.diff,
-                 assortativity.lobe=assort.lobe.diff)
-
-    } else if (level == 'lobe') {
-      g1 <- lapply(g1, assign_lobes, atlas.list)
-      g2 <- lapply(g2, assign_lobes, atlas.list)
-
-      t1 <- as.data.table(ldply(g1, count_interlobar, 'Temporal', atlas.list))
-      t2 <- as.data.table(ldply(g2, count_interlobar, 'Temporal', atlas.list))
-      tdiff <- t1 - t2
-      tmp <- data.table(density=densities, diff=tdiff)
-
-    } else if (level == 'asymmetry') {
-      g1 <- lapply(g1, assign_lobes, atlas.list)
-      g2 <- lapply(g2, assign_lobes, atlas.list)
-
-      asymm1 <- ldply(g1, edge_asymmetry)$asymm
-      asymm2 <- ldply(g2, edge_asymmetry)$asymm
-      adiff <- asymm1 - asymm2
-      tmp <- data.table(density=densities, asymm=adiff)
+      if (level == 'graph') {
+        mod1 <- sapply(g1, function(x) modularity(cluster_louvain(x)))
+        mod2 <- sapply(g2, function(x) modularity(cluster_louvain(x)))
+        mod.diff <- mod1 - mod2
+        Cp1 <- sapply(g1, transitivity, type='localaverage')
+        Cp2 <- sapply(g2, transitivity, type='localaverage')
+        Cp.diff <- Cp1 - Cp2
+        Lp1 <- sapply(g1, average.path.length)
+        Lp2 <- sapply(g2, average.path.length)
+        Lp.diff <- Lp1 - Lp2
+        assortativity1 <- sapply(g1, assortativity.degree)
+        assortativity2 <- sapply(g2, assortativity.degree)
+        assort.diff <- assortativity1 - assortativity2
+        E.global1 <- sapply(g1, graph.efficiency, 'global')
+        E.global2 <- sapply(g2, graph.efficiency, 'global')
+        E.global.diff <- E.global1 - E.global2
+  
+        assort.lobe1 <- sapply(g1, function(x) assortativity_nominal(x, V(x)$lobe))
+        assort.lobe2 <- sapply(g2, function(x) assortativity_nominal(x, V(x)$lobe))
+        assort.lobe.diff <- assort.lobe1 - assort.lobe2
+        tmp <- data.table(density=densities, mod=mod.diff, E.global=E.global.diff,
+                   Cp=Cp.diff, Lp=Lp.diff, assortativity=assort.diff,
+                   assortativity.lobe=assort.lobe.diff)
+  
+      } else if (level == 'lobe') {
+        t1 <- as.data.table(ldply(g1, count_interlobar, 'Temporal', atlas.dt))
+        t2 <- as.data.table(ldply(g2, count_interlobar, 'Temporal', atlas.dt))
+        tdiff <- t1 - t2
+        tmp <- data.table(density=densities, diff=tdiff)
+  
+      } else if (level == 'asymmetry') {
+        asymm1 <- ldply(g1, edge_asymmetry)$asymm
+        asymm2 <- ldply(g2, edge_asymmetry)$asymm
+        adiff <- asymm1 - asymm2
+        tmp <- data.table(density=densities, asymm=adiff)
+      }
     }
 
     tmp
