6125ab7ade7a2bd0772577b121d0c169ab1c8b96
Chris Watson <cgwatson@bu.edu>
2019-04-07 09:34:27 -0500

    Add new `make_auc_brainGraph` function

---
+++ R/auc.R
@@ -0,0 +1,71 @@
+#' Calculate the AUC across densities of given attributes
+#'
+#' Given a list of \code{brainGraphList} objects, this function will calculate
+#' the area under the curve (AUC) across all thresholds/densities for each
+#' subject or group.
+#'
+#' @param g.list A list of \code{brainGraphList} objects
+#' @param g.attr A character vector of graph attribute name(s). Default:
+#'   \code{NULL}
+#' @param v.attr A character vector of vertex attribute name(s). Default:
+#'   \code{NULL}
+#' @param norm Logical indicating whether to normalize threshold values to
+#'   between 0 and 1. Default: \code{FALSE}
+#' @export
+#' @return A \code{brainGraphList} object in which the graphs are for each
+#'   subject
+#'
+#' @examples
+#' \dontrun{
+#' g.auc <- make_auc_brainGraph(g.fa, g.attr='E.global.wt')
+#' }
+
+make_auc_brainGraph <- function(g.list, g.attr=NULL, v.attr=NULL, norm=FALSE) {
+  # Check if components are 'brainGraphList' objects
+  matches <- vapply(g.list, inherits, logical(1), 'brainGraphList')
+  if (any(!matches)) stop("Input must be a list of 'brainGraphList' objects.")
+
+  # Get the meta variables first
+  atlas <- g.list[[1]]$atlas
+  type <- g.list[[1]]$type
+  level <- g.list[[1]]$level
+  modality <- g.list[[1]]$modality
+  weighting <- g.list[[1]]$weighting
+
+  kNumThresh <- length(g.list)
+  if (!is.null(g.list[[1]]$threshold)) {
+    thresholds <- sapply(g.list, with, threshold)
+  } else {
+    thresholds <- seq(from=0, to=1, length.out=kNumThresh)
+  }
+  subjects <- names(g.list[[1]]$graphs)
+  kNumSubjs <- length(subjects)
+  g.auc <- foreach(i=seq_along(subjects)) %dopar% {
+    g.subj <- lapply(g.list, `[`, i)
+    g.tmp <- make_empty_brainGraph(atlas, type=type, level=level, modality=modality, weighting=weighting,
+                                   name=subjects[i], Group=g.list[[1]][i]$Group)
+    if (!is.null(g.attr)) {
+      for (k in seq_along(g.attr)) {
+        y <- sapply(g.subj, graph_attr, g.attr[k])
+        g.tmp <- set_graph_attr(g.tmp, g.attr[k], abs(auc_diff(thresholds, y)))
+      }
+    }
+    if (!is.null(v.attr)) {
+      for (k in v.attr) {
+        y <- t(sapply(g.subj, vertex_attr, k))
+        g.tmp <- set_vertex_attr(g.tmp, k, value=abs(auc_diff(thresholds, y)))
+      }
+    }
+    g.tmp
+  }
+  attrs <- c('atlas', 'type', 'level', 'modality', 'weighting')
+  out <- setNames(vector('list', length(attrs)), attrs)
+  for (a in attrs) {
+    if (!is.null(get(a))) out[[a]] <- get(a)
+  }
+  out <- get_metadata(out)
+  out$graphs <- g.auc
+  names(out$graphs) <- subjects
+  class(out) <- c('brainGraphList', 'list')
+  return(out)
+}
